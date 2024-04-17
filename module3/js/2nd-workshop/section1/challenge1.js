1bee71f65a923b441a5b793644acce6dacecc1f3
dagutttt <dagutmu667@gmail.com>
2024-04-16 21:47:21 -0500

    WIP: solving section 1 challenge 1

---
+++ module3/js/2nd-workshop/section1/challenge1.js
@@ -0,0 +1,30 @@
+const title = "Julian's trip";
+document.title = title;
+document.querySelector('h1').textContent = title;
+
+// -*******************************************************************************-
+// Exercise
+
+const destiny = prompt('Enter the destiny you want to go');
+
+let daysToTravel, budget;
+
+let canTravel = false;
+while (!canTravel) {
+    daysToTravel = Number(
+        prompt(`How many days are you planning to spend in ${destiny}?`)
+    );
+    budget = Number(prompt('Enter your budget for this trip'));
+
+    const TRIP_DAILY_COST = 500;
+    const tripTotalCost = daysToTravel * TRIP_DAILY_COST;
+    canTravel = tripTotalCost < budget;
+    if (!canTravel)
+        alert(
+            'You should either reconsider the number of days to travel or your budget for the trip'
+        );
+    else {
+        alert('Have a nice trip!');
+        break;
+    }
+}
