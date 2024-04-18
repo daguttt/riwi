196d9077ab3b67e794333bba760049ba840913bb
dagutttt <dagutmu667@gmail.com>
2024-04-17 21:49:08 -0500

    WIP feat(js/2nd): solve section 1 challenge 2

---
+++ module3/js/2nd-workshop/section1/challenge2.js
@@ -0,0 +1,102 @@
+const BUDGET = 500;
+
+function getValidatedInput(promptMessage, validateInputFn) {
+    while (true) {
+        const input = prompt(promptMessage);
+        const { isInputValid, validatedInput, invalidInputMessage } =
+            validateInputFn(input);
+        if (isInputValid) return validatedInput;
+
+        alert(invalidInputMessage);
+    }
+}
+
+function askForSouvenirs() {
+    const souvenirList = [];
+    while (true) {
+        const souvenirName = prompt('Ingresa el nombre del suvenir a comprar');
+        const souvenirCost = getValidatedInput(
+            'Ingresa el costo del suvenir',
+            (input) => {
+                const castedInput = Number(input);
+                return {
+                    validatedInput: castedInput,
+                    isInputValid: !isNaN(castedInput) && input > 0,
+                    invalidInputMessage:
+                        'El costo ingresado es invalido. Inténtalo de nuevo.',
+                };
+            }
+        );
+
+        const souvenirAvailability = confirm(
+            '¿El suvenir está disponible para comprar?'
+        );
+
+        const souvenir = {
+            name: souvenirName,
+            cost: souvenirCost,
+            availability: souvenirAvailability,
+        };
+
+        souvenirList.push(souvenir);
+
+        const wantToAddMoreSouvenirs = confirm('¿Quieres añadir otro suvenir?');
+        if (!wantToAddMoreSouvenirs) break;
+    }
+    return souvenirList;
+}
+
+function formatSouvenirList(souvenirList) {
+    return souvenirList.map((souvenir, index) => {
+        return `${index}. Nombre: ${souvenir.name}. Costo: ${souvenir.cost}`;
+    });
+}
+
+function showSouvernirsToBuy(souvenirList) {
+    console.log('SUVENIRS DISPONIBLES');
+    const formattedSouvenirListToShow = formatSouvenirList(souvenirList);
+    const souvenirIndexesToBuyString = prompt(`
+${formattedSouvenirListToShow.join('\n')}
+
+Ingresa el numero de los suvenires que quieres comprar separados por comma ( , ):`);
+
+    const souvenirIndexesToBuy = souvenirIndexesToBuyString
+        .replace(' ', '')
+        .split(',')
+        .map((souvenirIndex) => Number(souvenirIndex));
+
+    const souvenirListToBuy = souvenirList.filter((souvenir, index) =>
+        souvenirIndexesToBuy.includes(index)
+    );
+
+    console.log({
+        // souvenirIndexesToBuyString,
+        // souvenirIndexesToBuy,
+        souvenirListToBuy,
+    });
+}
+
+const mockedSouvenirList = [
+    {
+        name: 'Lápiz',
+        cost: 15,
+        availability: false,
+    },
+    {
+        name: 'Camiseta',
+        cost: 150,
+        availability: true,
+    },
+    {
+        name: 'Vaso',
+        cost: 50,
+        availability: true,
+    },
+];
+
+// const souvenirList = askForSouvenirs();
+const availableSouvenirList = mockedSouvenirList.filter(
+    (souvenir) => souvenir.availability
+);
+
+showSouvernirsToBuy(mockedSouvenirList);
