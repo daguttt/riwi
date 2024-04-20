bcf0da9d83c7e09675e538c28fc63c3a91e51659
daguttt <dagutmu667@gmail.com>
2024-04-19 21:49:56 -0500

    WIP feat(js/2nd): solving section 1 challenge 3

--- module3/js/2nd-workshop/section1/challenge3.js
+++ module3/js/2nd-workshop/section1/challenge3.js
@@ -1,22 +1,11 @@
-const desirableItems = [];
-
 // -*********************************************************-
 // Utils
 // -*********************************************************-
-
-function getValidatedInput(promptMessage, validateInputFn) {
+function askForCheckedAmount(promptMessage) {
     while (true) {
-        const input = prompt(promptMessage);
-        const validityObject = {
-            validatedInput,
-            isInputValid,
-            invalidInputMessage,
-        };
-        const { validatedInput, isInputValid, invalidInputMessage } =
-            validateInputFn(input, validityObject);
-        if (isInputValid) return validatedInput;
-
-        alert(invalidInputMessage);
+        const input = Number(prompt(promptMessage));
+        if (!Number.isNaN(input) && input > 0) return input;
+        alert('Invalid input value. Try again.');
     }
 }
 
@@ -24,10 +13,49 @@ function getValidatedInput(promptMessage, validateInputFn) {
 // Use cases
 // -*********************************************************-
 
-function askForDesirableItems() {}
+function getBudget({ estimatedTotalToSpend }) {
+    while (true) {
+        const budget = askForCheckedAmount('Enter your budget');
+        if (estimatedTotalToSpend < budget) return budget;
+        alert('You need more money for your trip.');
+    }
+}
+
+function askForDesirableItems() {
+    const desirableItems = [];
+    let wantToAddMoreItems = true;
+    while (wantToAddMoreItems) {
+        const itemName = prompt(
+            'Enter the name of the item you would like to buy'
+        );
+        const itemPrice = prompt(`How much is that ${itemName}?`);
+        desirableItems.push({ name: itemName, price: itemPrice });
+        wantToAddMoreItems = confirm('¿Quieres añadir otro suvenir?');
+    }
+    return desirableItems;
+}
 
 function main() {
-    const budget = getValidatedInput('');
+    const expenses = {
+        lodging: askForCheckedAmount('How much does your lodging cost?'),
+        transport: askForCheckedAmount('How much will your transport cost?'),
+        food: askForCheckedAmount('What is your budget for food?'),
+    };
+    const expensesTotal = Object.values(expenses).reduce(
+        (total, expenseValue) => total + expenseValue
+    );
+    const budget = getBudget({ estimatedTotalToSpend: expensesTotal });
+
+    const emergencyBudget = askForCheckedAmount(
+        'How much are you planning to save for emergencies?'
+    );
+    let remainingBudget = budget - expensesTotal;
+    if (remainingBudget < emergencyBudget)
+        return alert('Avoid buying additional items to exceed your budget');
+
+    const desirableItems = askForDesirableItems();
+    const cheapestItem = getCheapestItem();
+    // if (cheapestItem.price > remainingBudget)
 }
 
 main();
