c927562d64f6da31b9d6f29f7a06d7afbabb4356
daguttt <dagutmu667@gmail.com>
2024-04-21 21:37:59 -0500

    feat(js/2nd): solve section 2 challenge 2

--- module3/js/2nd-workshop/section2/challenge2.js
+++ module3/js/2nd-workshop/section2/challenge2.js
@@ -1,6 +1,12 @@
 // -*********************************************************-
 // Utils
 // -*********************************************************-
+const amountFormatter = new Intl.NumberFormat('es-US', {
+    style: 'currency',
+    currency: 'USD',
+    currencyDisplay: 'narrowSymbol',
+});
+
 function changePageTitle() {
     return new Promise((resolve) => {
         const title = 'Calculadora de Presupuesto';
@@ -10,8 +16,74 @@ function changePageTitle() {
     });
 }
 
+function askForCheckedAmount(promptMessage) {
+    while (true) {
+        const input = Number(prompt(promptMessage));
+        if (!Number.isNaN(input) && input > 0) return input;
+        alert('Valor ingresado inválido. Intenta de nuevo.');
+    }
+}
+
+// -*********************************************************-
+// Use cases
+// -*********************************************************-
+
+function askForDailyMinSavings({ dailyBudget }) {
+    while (true) {
+        const dailyMinSavings = askForCheckedAmount(
+            `Tu presupuesto diario es: ${dailyBudget}\n` +
+                'Ingresa cuánto quieres ahorrar'
+        );
+        if (dailyMinSavings <= dailyBudget) return dailyMinSavings;
+        alert(
+            `No puedes ahorrar más de tu presupesto diario. Presupuesto diario: ${amountFormatter.format(
+                dailyBudget
+            )}`
+        );
+    }
+}
+
 async function main() {
     await changePageTitle();
+    const dailyBudget = askForCheckedAmount('¿Cuál es tu presupuesto diario?');
+    const dailyMinSavings = askForDailyMinSavings({ dailyBudget });
+
+    let remainingBudget = dailyBudget - dailyMinSavings;
+
+    const eatingOutsideCost = askForCheckedAmount(
+        '¿Cuanto te cuesta comer afuera?'
+    );
+    const buyingBooksCost = askForCheckedAmount(
+        '¿Cuanto te cuesta comprar libros?'
+    );
+
+    const prefersBuyingBooks = confirm('¿Prefieres comprar libros?');
+
+    let canBuyBooks;
+    let canEatOutside;
+    if (prefersBuyingBooks) {
+        canBuyBooks = buyingBooksCost <= remainingBudget;
+        remainingBudget -= buyingBooksCost;
+        canEatOutside = eatingOutsideCost <= remainingBudget;
+    } else {
+        canEatOutside = eatingOutsideCost <= remainingBudget;
+        remainingBudget -= eatingOutsideCost;
+        canBuyBooks = buyingBooksCost <= remainingBudget;
+    }
+
+    if (canEatOutside && canBuyBooks) {
+        console.log(
+            'Puedes comprar libros y comer afuera sin afectar tus ahorros'
+        );
+    } else if (canEatOutside) {
+        console.log('Puedes comer afuera sin afectar tus ahorros');
+    } else if (canBuyBooks) {
+        console.log('Puedes comprar libros sin afectar tus ahorros');
+    } else {
+        console.log(
+            'No te puedes permitir gastar en ninguna actividad en el momento'
+        );
+    }
 }
 
 main();
