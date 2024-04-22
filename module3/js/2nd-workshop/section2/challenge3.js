f880716173b751300845054fcf00626a10c8a462
daguttt <dagutmu667@gmail.com>
2024-04-21 23:27:22 -0500

    feat(js/2nd): solve section 2 challenge 3

---
+++ module3/js/2nd-workshop/section2/challenge3.js
@@ -0,0 +1,80 @@
+// -*********************************************************-
+// Constants
+// -*********************************************************-
+const WEATHERS = Object.freeze({
+    SUNNY: 'Soleado',
+    RAINY: 'Lluvioso',
+});
+const weatherOptionList = Object.values(WEATHERS);
+
+// -*********************************************************-
+// Utils
+// -*********************************************************-
+function changePageTitle() {
+    return new Promise((resolve) => {
+        const title = 'Decisiones de Última Hora';
+        document.title = title;
+        document.querySelector('h1').textContent = title;
+        resolve();
+    });
+}
+
+function askForNumber(promptMessage) {
+    while (true) {
+        const input = Number(prompt(promptMessage));
+        if (!Number.isNaN(input)) return input;
+        alert('El valor ingresado no es número');
+    }
+}
+
+// -*********************************************************-
+// Use cases
+// -*********************************************************-
+function askForForecast() {
+    const separation = '\n';
+    const formattedOptions = weatherOptionList.map(
+        (o, index) => `${index}. ${o}.`
+    );
+    while (true) {
+        const weatherOption = askForNumber(
+            `¿Cuál es el pronóstico del climea?:${separation}` +
+                formattedOptions.join(separation)
+        );
+        if (weatherOption in weatherOptionList)
+            return weatherOptionList[weatherOption];
+        alert(
+            'La opción que ingresaste es inválida o no está disponible en la lista.'
+        );
+    }
+}
+
+async function main() {
+    await changePageTitle();
+    const forecast = askForForecast();
+    const hasAvailableSpaceInBag = confirm(
+        '¿Tienes espacio disponible en la maleta?'
+    );
+
+    if (!hasAvailableSpaceInBag) {
+        return alert('No puedes llevar más articulos');
+    }
+
+    const bagWeigthLimit = askForNumber(
+        '¿Cuál es el limite de peso permito para la maleta?'
+    );
+
+    if (bagWeigthLimit > 20) {
+        alert('Puedes llevar la cámara y un paraguas en caso de que llueva.');
+    } else if (bagWeigthLimit > 15 && bagWeigthLimit <= 20) {
+        if (forecast === WEATHERS.SUNNY) alert('Podrías llevar una cámara.');
+        else alert('Considera llevar un paraguas');
+    } else {
+        if (forecast === WEATHERS.SUNNY)
+            alert(
+                'Asegurate de llevar todo lo necesario para protegete del sol'
+            );
+        else alert('Prioriza ropa para ocaciones de lluvia.');
+    }
+}
+
+main();
