83f972f3e207d828dae6abe59924959e0490183a
daguttt <dagutmu667@gmail.com>
2024-04-24 11:22:34 -0500

    feat(js/3rd): solve section 2 challenge 1

---
+++ module3/js/3rd-workshop/section2/challenge1/index.js
@@ -0,0 +1,35 @@
+// -*********************************************************-
+// Utils
+// -*********************************************************-
+
+// -*********************************************************-
+// Use cases
+// -*********************************************************-
+function askForGrades() {
+    const promptMessage =
+        'Calculadora de Promedio de Calificaciones\n\n' +
+        'Ingresa tus notas separadas por comas:';
+    const mockedGradesInput = '2, 3.5, 4, 5, 1, 1.5';
+    while (true) {
+        const gradesInput = prompt(promptMessage);
+        if (gradesInput) return gradesInput.replace(' ', '').split(',');
+
+        if (gradesInput === null) {
+            alert('Calculadora cerrada');
+            return null;
+        }
+        if (gradesInput === '')
+            alert('Notas no ingresadas. Inténtalo de nuevo');
+    }
+}
+
+function main() {
+    const gradeList = askForGrades();
+    if (!gradeList) return;
+    const gradesTotal = gradeList
+        .map(Number)
+        .reduce((total, grade) => total + grade);
+    const average = (gradesTotal / gradeList.length).toFixed(2);
+    alert(`El promedio de tus calificaciones es: ${average}`);
+}
+main();
