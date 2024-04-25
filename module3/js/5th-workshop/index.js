13da5e69d96b6a51c977bbb0d2a25c72e0a9d979
daguttt <dagutmu667@gmail.com>
2024-04-25 12:12:54 -0500

    feat(js/5th): solve cases 2, 3, 4

--- module3/js/5th-workshop/index.js
+++ module3/js/5th-workshop/index.js
@@ -1,7 +1,8 @@
-const amountFormatter = new Intl.NumberFormat('es-CO', {
+const amountFormatter = new Intl.NumberFormat('es-US', {
     currency: 'COP',
     style: 'currency',
 });
+
 let budget = 2_500_000;
 
 // -*********************************************************-
@@ -20,11 +21,11 @@ function showMessage(message) {
 }
 
 let timesShowedBudget = 0;
-function showCurrentBudget(currentBudget) {
+function showCurrentBudget() {
     showMessage(
-        `${++timesShowedBudget}. Presupuesto: ${amountFormatter.format(
-            budget
-        )}\n`
+        `${++timesShowedBudget}. Presupuesto: <span class="blue">
+            ${amountFormatter.format(budget)}
+        </span>\n`
     );
 }
 
@@ -74,12 +75,21 @@ function case1() {
     switch (option) {
         case 1:
             budget -= 15_000;
+            break;
         case 2:
             budget -= 23_000;
+            break;
     }
-    showMessage(optionsToBuy[optionIndex].title);
-    showMessage(optionsToBuy[optionIndex].message);
-    showCurrentBudget(budget);
+    const optionToBuy = optionsToBuy[optionIndex];
+    const priceMessage = optionToBuy.price
+        ? `Vale: <span class="red">${amountFormatter.format(
+              optionToBuy.price
+          )}</span>`
+        : '';
+
+    showMessage(`${optionToBuy.title} ${priceMessage}`);
+    showMessage(optionToBuy.message);
+    showCurrentBudget();
 }
 
 // -*********************-
@@ -96,9 +106,67 @@ function case2() {
         length: 40,
         width: 20,
     };
+    const reductionFactorHeight =
+        maxSuitcaseSizes.height / suitcaseSizes.height;
+    const reductionFactorLength =
+        maxSuitcaseSizes.length / suitcaseSizes.length;
+    const reductionFactorWidth = maxSuitcaseSizes.width / suitcaseSizes.width;
+    const minReductionFactor = Math.min(
+        reductionFactorHeight,
+        reductionFactorLength,
+        reductionFactorWidth
+    );
+
+    Object.assign(suitcaseSizes, {
+        height: Number((suitcaseSizes.height * minReductionFactor).toFixed(2)),
+        length: Number((suitcaseSizes.length * minReductionFactor).toFixed(2)),
+        width: Number((suitcaseSizes.width * minReductionFactor).toFixed(2)),
+    });
+    showMessage(
+        'Las nuevas medidas de la maleta son<br>' +
+            `Alto: ${suitcaseSizes.height}<br>` +
+            `Largo: ${suitcaseSizes.length}<br>` +
+            `Ancho: ${suitcaseSizes.width}<br>`
+    );
+}
+
+// -*********************-
+// Case 3
+function case3() {
+    const WIFI_COST_HOURLY = 50_000;
+    const airportPasswordInBinary = '01110010_01101001_01110111_01101001';
+    const numbersAsDecimals = airportPasswordInBinary
+        .split('_')
+        .map((binaryNumber) => parseInt(binaryNumber, 2));
+    const airportPassword = numbersAsDecimals.reduce(
+        (password, number) => password + String.fromCharCode(number),
+        ''
+    );
+
+    budget -= WIFI_COST_HOURLY;
+    showMessage(
+        `La contraseña desencriptada es: <code>${airportPassword}</code>. La hora vale: <span class="red">
+            ${amountFormatter.format(WIFI_COST_HOURLY)}
+        </span>`
+    );
+    showCurrentBudget();
+}
+
+// -*********************-
+// Case 4
+function case4() {
+    const rawMessage = 'Taxi me puede llevar al hotel mariposas amarillas';
+    const formattedMessage = rawMessage.replace(/[aeiou]/g, 'i');
+    showMessage(
+        'Puedes decir el siguiente mensaje para pedir el taxi:<br>' +
+            `<i>"${formattedMessage}"</i>`
+    );
 }
 
 function main() {
     case1();
+    case2();
+    case3();
+    case4();
 }
 main();
