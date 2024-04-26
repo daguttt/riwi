ec440e0ac259256eed3079df1867f41aef5be8ed
daguttt <dagutmu667@gmail.com>
2024-04-25 21:47:52 -0500

    chore(js/3rd): add inventory feature list with its functions

--- module3/js/3rd-workshop/section2/challenge3/index.js
+++ module3/js/3rd-workshop/section2/challenge3/index.js
@@ -1,3 +1,6 @@
+const products = [];
+const blacklistedProducts = [];
+
 // -*********************************************************-
 // Utils
 // -*********************************************************-
@@ -12,52 +15,77 @@ function askForNumber(promptMessage) {
 // -*********************************************************-
 // Use cases
 // -*********************************************************-
+const addProduct = function () {};
+
+const duplicateProduct = function () {};
+
+const listProductsInInventory = function () {};
+
+const searchProducts = function () {};
+
+const updateProduct = function () {};
+
+const updateProductQuantity = function () {};
+
+const removeProduct = function () {};
+
+const checkProductAvailability = function () {};
+
+const sellProduct = function () {};
+
+const getInventoryTotalPrice = function () {};
+
+const generateGeneralReport = function () {};
+
+const inventoryFeatures = [
+    {
+        title: 'Agregar un producto.',
+        featureFn: addProduct,
+    },
+    {
+        title: 'Duplicar un producto.',
+        featureFn: duplicateProduct,
+    },
+    {
+        title: 'Ver productos en el inventario.',
+        featureFn: listProductsInInventory,
+    },
+    {
+        title: 'Buscar productos (Nombre o Precio).',
+        featureFn: searchProducts,
+    },
+    { title: 'Actualizar un producto.', featureFn: updateProduct },
+    {
+        title: 'Actualizar cantidad un producto. (Compra de inventario)',
+        featureFn: updateProductQuantity,
+    },
+    { title: 'Eliminar un producto.', featureFn: removeProduct },
+    {
+        title: 'Verificar existencia de un producto.',
+        featureFn: checkProductAvailability,
+    },
+    { title: 'Vender un producto.', featureFn: sellProduct },
+    {
+        title: 'Ver valor total del inventario',
+        featureFn: getInventoryTotalPrice,
+    },
+    { title: 'Generar reporte general', featureFn: generateGeneralReport },
+    { title: 'Salir.', featureFn: null },
+];
 
 function main() {
     let menuOpened = true;
     let id = 0;
     while (menuOpened) {
-        const optionMessageLines = [
-            'Agregar un producto.',
-            'Duplicar un producto.',
-            'Ver productos en el inventario.',
-            'Buscar productos (Nombre o Precio).',
-            'Actualizar un producto.',
-            'Actualizar cantidad un producto. (Compra de inventario)',
-            'Eliminar un producto.',
-            'Verificar existencia de un producto.',
-            'Vender un producto.',
-            'Ver valor total del inventario',
-            'Salir.',
-        ];
         const option = prompt(
             'Bienvenido a tu sistema de gestión de inventario:\n' +
-                optionMessageLines
-                    .map((line, index) => `${index + 1}. ${line}`)
+                inventoryFeatures
+                    .map((option, index) => `${index + 1}. ${option.title}`)
                     .join('\n')
         );
-        const lastLineIndex = optionMessageLines.length - 1;
-        switch (option) {
-            case lastLineIndex: {
-                menuOpened = false;
-                break;
-            }
-            case '1': {
-                break;
-            }
-            case '2': {
-                break;
-            }
-            case '3': {
-                break;
-            }
-            case '4': {
-                break;
-            }
-            case '5': {
-                break;
-            }
-        }
+
+        const featureIndex = option - 1;
+        inventoryFeatures[featureIndex].featureFn;
     }
 }
 
