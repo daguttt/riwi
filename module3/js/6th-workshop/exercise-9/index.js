b454f4486fd3b2c9474ffc1681f1d5bf2739f7e8
daguttt <dagutmu667@gmail.com>
2024-05-03 10:26:28 -0500

    feat(js/6th): solve exercise 9

---
+++ module3/js/6th-workshop/exercise-9/index.js
@@ -0,0 +1,32 @@
+// -*********************************************************-
+// Utils
+// -*********************************************************-
+function askForNumber(promptMessage, options = {}) {
+    while (true) {
+        const rawInput = prompt(promptMessage);
+        if (options?.allowedNullish && !rawInput) return rawInput;
+        const castedInput = parseInt(rawInput);
+        if (!Number.isNaN(castedInput)) return castedInput;
+        alert('El valor ingresado no es número');
+    }
+}
+
+// -*********************************************************-
+// Use cases
+// -*********************************************************-
+
+function main() {
+    const delay = askForNumber(
+        '¿Cuánto tiempo quieres que se demore para mostrar los datos?'
+    );
+    setTimeout(() => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then((res) => {
+                if (res.ok) return res.json();
+                throw new Error('Error en la petición de los datos');
+            })
+            .then(console.log)
+            .catch((err) => console.error(err));
+    }, delay);
+}
+main();
