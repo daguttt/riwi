7051e76bea6c305ca19d1d901a074025d19a57c0
daguttt <dagutmu667@gmail.com>
2024-05-10 11:55:10 -0500

    feat(js/7th): structure online points store system

---
+++ module3/js/7th-workshop/part-2/section-2/online-points-store.js
@@ -0,0 +1,102 @@
+function ProductCategory(name, description, products) {
+    this.name = name;
+    this.description = description;
+    this.products = products;
+}
+ProductCategory.prototype.listProduct = function () {
+    this.products.forEach((product) => console.log(product.getInfo()));
+};
+
+function Product(name, points, stock) {
+    this.name = name;
+    this.points = points;
+    this.stock = stock;
+}
+Product.prototype.getInfo = function () {
+    return `${this.name} - ${this.points} - ${this.stock}`;
+};
+
+function PhysicalProduct(name, points, stock, price) {
+    Product.call(this, name, points, stock);
+    this.price = price;
+}
+
+PhysicalProduct.prototype = Object.create(Product.prototype);
+PhysicalProduct.prototype.constructor = PhysicalProduct;
+PhysicalProduct.prototype.updateStock = function (newStock) {
+    console.log('Actualizando stock');
+    this.stock = newStock;
+};
+PhysicalProduct.prototype.sendProduct = function (user) {
+    console.log(`Enviando producto ${this.name} a '${user.name}'`);
+};
+
+function DigitalProduct(name, points, stock, url) {
+    Product.call(this, name, points, stock);
+    this.url = url;
+}
+
+DigitalProduct.prototype = Object.create(Product.prototype);
+DigitalProduct.prototype.constructor = DigitalProduct;
+DigitalProduct.prototype.download = function () {
+    console.log(`Descargando '${this.name}'...`);
+};
+DigitalProduct.prototype.getProduct = function () {
+    console.log(`Obteniendo '${this.name}'...`);
+};
+
+function Person(name, email, password) {
+    this.name = name;
+    this.email = email;
+    this.password = password;
+}
+
+function User(name, email, password, initialPoints = 0) {
+    Person.call(this, name, email, password);
+    this.points = initialPoints;
+}
+
+User.prototype = Object.create(Person.prototype);
+User.prototype.constructor = User;
+User.prototype.accumulatePoints = function (points) {
+    console.log(`Acumulando ${points} puntos.`);
+    this.points += points;
+};
+User.prototype.exchangePoints = function (points) {
+    console.log(`Cajeando ${points} puntos.`);
+    this.points -= points;
+};
+
+function Admin(name, email, password) {
+    Person.call(this, name, email, password);
+}
+
+Admin.prototype = Object.create(Person.prototype);
+Admin.prototype.constructor = Admin;
+Admin.prototype.addProduct = function (product) {
+    console.log(`Agregando ${product.name} al inventario.`);
+};
+
+function ExchangeOrder(user, product, date) {
+    this.user = user;
+    this.product = product;
+    this.date = date;
+}
+ExchangeOrder.prototype.confirmOrder = function () {
+    console.log(
+        `Confirmando pedido de '${this.product.name}' para ${this.user.name}.`
+    );
+};
+ExchangeOrder.prototype.cancelOrder = function () {
+    console.log(
+        `Cancelando pedido de '${this.product.name}' para ${this.user.name}.`
+    );
+};
+
+function Activity(type, awardedPoints) {
+    this.type = type;
+    this.awardedPoints = awardedPoints;
+}
+Activity.prototype.complete = function () {
+    console.log(`Completando actividad de '${this.type}'.`);
+};
